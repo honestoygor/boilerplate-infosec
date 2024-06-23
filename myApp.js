9baa445d05b1111dbbb1bb2d086f8a0186a538a8
Ygor Honesto <honestoygor@gmail.com>
2024-06-23 16:37:38 +0000

    Changed myApp.js

--- myApp.js
+++ myApp.js
@@ -57,5 +57,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`Useful Information: Your app is listening on port ${port}`);
 });
